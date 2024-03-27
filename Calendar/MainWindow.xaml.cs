3c1794e99e3404a330481478ddf35d2408b9e2c7
Miiapetri <101567276+Miiapetri@users.noreply.github.com>
2024-03-27 11:00:17 +0200

    getting text from a class

--- Calendar/MainWindow.xaml.cs
+++ Calendar/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,47 @@ namespace Calendar
     /// </summary>
     public partial class MainWindow : Window
     {
+        DateInfo myDateInfo;
         public MainWindow()
         {
+            myDateInfo = new DateInfo()
+            {
+                SelectedDate = DateTime.Now,
+            };
             InitializeComponent();
+
+            textPanel.DataContext = myDateInfo;
+        }
+
+        public class DateInfo : INotifyPropertyChanged
+        {
+            private DateTime? selectedDate;
+
+            public DateTime? SelectedDate
+            {
+                get
+                {
+                    return this.selectedDate;
+                }
+                set
+                {
+                    if (this.selectedDate != value)
+                    {
+                        this.selectedDate = value;
+                        pChanged("SelectedDate");
+                    }
+                }
+            }
+
+            protected virtual void pChanged(string pName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(pName));
+                }
+
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
         }
     }
 }
